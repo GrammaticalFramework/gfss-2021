ee6dec76a47120290fe3f4f4bd52705fa16d43e4
Meng Weng Wong <mengwong@gmail.com>
2021-08-02 15:17:17 +0800

    working sentence
        *Main> someFunc
        upon sunrise , the organisation must make an assessment as soon as practicable .

--- day-6/complaw/src/Lib.hs
+++ day-6/complaw/src/Lib.hs
@@ -1,3 +1,4 @@
+{-# LANGUAGE ViewPatterns #-}
 module Lib
     ( someFunc
     ) where
@@ -6,32 +7,53 @@ import CompLaw
 import PGF hiding (Tree)
 import qualified Data.Map.Strict as Map
 
-data L4Sentence = L4S [Condition] Event [Party] Deontic Action Deadline deriving (Show, Eq, Read)
+data L4Sentence = L4S L4Event [Party] L4Deontic L4Action L4Deadline deriving (Show, Eq, Read)
 data Condition  = Condition String [String]                             deriving (Show, Eq, Read) -- Timed Automata
-data Event      = Event String                                          deriving (Show, Eq, Read) -- Timed Automata
+data L4Event    = L4Event String                                        deriving (Show, Eq, Read) -- Timed Automata
 data Party      = Party String                                          deriving (Show, Eq, Read)
-data Deontic    = Must | May                                            deriving (Show, Eq, Read)
-data Action     = Action { name :: String
-                         , params :: Map.Map String String }          deriving (Show, Eq, Read)
+data L4Deontic  = DMust | DMay                                          deriving (Show, Eq, Read)
+data L4Action   = L4Action { name :: String
+                           , params :: Map.Map String String }          deriving (Show, Eq, Read)
 -- pay(to=bob, some=money)
 -- notify(to=bob, body="you owe me money")
-data Deadline = Now | Then String                                       deriving (Show, Eq, Read) -- Timed Automata
+data L4Deadline = L4Now | L4Then String                                       deriving (Show, Eq, Read) -- Timed Automata
 
+event2gf (L4Event str) = Upon (LexKind str)
+
+party2gf (Party "PDPC") = PDPC
+party2gf (Party x) = TheSgKind (LexKind x)
+
+deontic2gf DMust = LexModal "Must"
+deontic2gf DMay  = LexModal "May"
+
+action2gf (L4Action name (Map.null -> True)) = fg $ mkApp (mkCId name) []
+action2gf (L4Action name (Map.toList -> [])) = fg $ mkApp (mkCId name) []
+
+deadline2gf (L4Now)    = LexDeadline "Now"
+deadline2gf (L4Then _) = LexDeadline "AsSoonAsPrac"
+
+-- CompLaw> l SProp (Pred (TheSgKind Organisation) (ModalAction Must (Use MakeAssessment)))
+-- the organisation must make an assessment .
 
 mySentence1 :: L4Sentence
 mySentence1 =
   L4S
-  []
-  (Event "upon sunrise")
+  (L4Event "Sunrise")
   [(Party "Organisation")]
-  Must
-  (Action "notify" (Map.fromList [("who","affected individuals")]))
-  Now
+  DMust
+  (L4Action "MakeAssessment" Map.empty)
+  (L4Then "soon")
 
-instance Gf Party where
-  gf (Party "Organisation") = gf GOrganisation
-  gf (Party x) = mkApp (mkCId x) []
-  fg = undefined
-
+sentence2gf :: L4Sentence -> Expr
+sentence2gf (L4S event (party:ps) l4d acts dline) =
+  gf $ SProp (event2gf event) (Pred (party2gf party) (ModalAction (deontic2gf l4d) (action2gf acts))) (deadline2gf dline)
+-- SProp Always (Pred PDPC (ModalAction Should MakeAssessment)) AsSoonAsPrac
+  
 someFunc :: IO ()
-someFunc = putStrLn (show mySentence1)
+someFunc = do
+  mypgf <- readPGF "src/CompLaw.pgf"
+  let myeng = head $ languages mypgf
+      mylin = linearise mypgf myeng (sentence2gf mySentence1)
+  putStrLn mylin
+  
+  where linearise = linearize
