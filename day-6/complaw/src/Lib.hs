aa28e69299d9a550429c8950416585cd36018cc0
Meng Weng Wong <mengwong@gmail.com>
2021-08-02 14:16:23 +0800

    beginnings of working Party GF

--- day-6/complaw/src/Lib.hs
+++ day-6/complaw/src/Lib.hs
@@ -12,10 +12,26 @@ data Event      = Event String                                          deriving
 data Party      = Party String                                          deriving (Show, Eq, Read)
 data Deontic    = Must | May                                            deriving (Show, Eq, Read)
 data Action     = Action { name :: String
-                         , params :: [Map.Map String String] }          deriving (Show, Eq, Read)
+                         , params :: Map.Map String String }          deriving (Show, Eq, Read)
 -- pay(to=bob, some=money)
 -- notify(to=bob, body="you owe me money")
 data Deadline = Now | Then String                                       deriving (Show, Eq, Read) -- Timed Automata
 
+
+mySentence1 :: L4Sentence
+mySentence1 =
+  L4S
+  []
+  (Event "upon sunrise")
+  [(Party "Organisation")]
+  Must
+  (Action "notify" (Map.fromList [("who","affected individuals")]))
+  Now
+
+instance Gf Party where
+  gf (Party "Organisation") = gf GOrganisation
+  gf (Party x) = mkApp (mkCId x) []
+  fg = undefined
+
 someFunc :: IO ()
-someFunc = putStrLn "someFunc"
+someFunc = putStrLn (show mySentence1)
